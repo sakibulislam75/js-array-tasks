8b2773e73a4e05ef7609ec824b1c8ba5a522ddc0
sakibulislam75 <sakibulislam1623@gmail.com>
2026-02-03 11:49:36 +0600

    push-pop problem

---
+++ Add or remove elements.js
@@ -0,0 +1,13 @@
+/*
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output
+*/
+
+let destination = ['paharpur', 'sajek', 'shylet'];
+destination.push('rangamati');
+destination.push('cox-bazar', 'saint-martin');
+destination.pop();
+console.log("The final array after adding and removing the elements:", destination);
