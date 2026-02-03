ad59a270b56a30a9545864b09660e4d9148b4411
sakibulislam75 <sakibulislam1623@gmail.com>
2026-02-03 12:21:17 +0600

    includes method problem

---
+++ membershiCheck.js
@@ -0,0 +1,10 @@
+/*
+3. Checking Array Membership with ‘includes’
+Instructions:
+Create an array of books containing different book.
+Use the includes method to check if the array contains a javascript book.
+Print a message to the console indicating whether the element is present in the array or not. 
+*/
+
+let books = ['c book', 'ph book', 'javascript book'];
+console.log("check ths js book present in the array:", books.includes('javascript book'));
