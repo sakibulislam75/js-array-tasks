0ec16831a6bd397e44781d88491093aee2d3b636
sakibulislam75 <sakibulislam1623@gmail.com>
2026-02-03 11:42:43 +0600

    first problem decalre and array push

---
+++ Declare an array.js
@@ -0,0 +1,11 @@
+/*
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
+
+let fruits = ['mango', 'banana', 'apple', 'orange', 'lichi'];
+console.log(fruits[3]);
+fruits[2] = "jambura";
+console.log("The final array:", fruits);
